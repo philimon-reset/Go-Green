a15e20443a5fee52d3436697c89cedf7f0932a44
mo-shaaban <mo.shaaban2000@gmail.com>
2022-11-05 23:39:15 +0100

    merge code

--- Server/src/routes/bounty.js
+++ Server/src/routes/bounty.js
@@ -10,12 +10,5 @@ bountyRouter.route("/user_id/").get( bountyController.getByUserId)
 bountyRouter.route('/approveBounty/:bountyId').patch( bountyController.approveBounty);
 bountyRouter.route('/approveClaim/').patch(bountyController.approveClaim);
 bountyRouter.route('/claim/:bountyId').post(checkOwnerClaim, bountyController.addClaim).delete(checkOwnerClaim, bountyController.removeClaim);
-// bountyRouter.route(checkauthenticated, '/approve_bounty/:approvedUserId').post(bountyController.approvedBounty);
-
-// bountyRouter.route('/confirmPlant'.post(bountyController.approveBounty));
-// bountyRouter.route(checkauthenticated, '/claim_bounty/:bountyId').get(bountyController.addClaim).delete(bountyController.removeClaim);
-// bountyRouter.route(checkauthenticated, '/approve_bounty/:approvedUserId').post(bountyController.approvedBounty);
-    // req.files is array of `photos` files
-    // req.body will contain the text fields, if there were any
 
 module.exports = bountyRouter;
