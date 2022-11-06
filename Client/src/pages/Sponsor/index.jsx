c45b3937838b45502a27895bfd7defdd73757bbc
Vahid <noreply+vahid@objectbox.io>
2022-11-06 06:37:17 +0100

    suggestions and starting planting dynamic route

--- Client/src/pages/Sponsor/index.jsx
+++ Client/src/pages/Sponsor/index.jsx
@@ -2,11 +2,12 @@ import "./style.css";
 
 const Sponsor = () => {
   const showSuggestions = (e) => {
-    let suggestionBox = document.getElementById("suggestions");
-    let inputBox = document.getElementById("tree-input");
+    
+    console.log(e.target.value)
+    let recommendations = document.getElementById('suggestions')
 
-    let inputData = e.target.value;
-    let emptyArray = [];
+    let inputData = e.target.value
+    let emptyArray = []
     let suggestions = [
       "Channel",
       "CodingLab",
@@ -37,62 +38,82 @@ const Sponsor = () => {
     ];
 
     if (inputData) {
+      
       emptyArray = suggestions.filter((data) => {
         return data
           .toLocaleLowerCase()
           .startsWith(inputData.toLocaleLowerCase());
       });
 
-      emptyArray = emptyArray.map((data) => {
-        return (data = "<li>" + data + "<li>");
-      });
+      emptyArray = emptyArray.map((data, index) => {
+        return data = 
+        `
 
-      console.log(emptyArray);
+        <li className="tree-result-container">
+          <img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Tree" className="tree-img" />
+          <div className="tree-details">
+              <div className="tree-name">Girar</div>
+              <div className="tree-minimum-price">$10</div>
+          </div>
+          <div className="suggestion-items" index="${index}"> ${data} </div>
+        </li>
+
+        `
+
+      })
+
+
+      console.log(emptyArray)
+
+      if(!emptyArray.length){
+        emptyArray.push('<li className="search-item" new-string="' + inputData + '" >' + inputData + '</li>')
+      }
+
+
+      recommendations.classList.add('open')
+    }else{
+      let list = document.querySelectorAll('li')
+      list.forEach((item) => {item.remove()})
     }
-  };
 
-  const toggleSuggestions = () => {
-    let suggestions = document.getElementById("suggestions");
-    suggestions.classList.toggle("open");
-  };
+    toggleSuggestions(emptyArray)
+  }
+
+ 
+
+  const toggleSuggestions = (list) => {
+    let listData
+    let suggestions = document.getElementById('suggestions')
+    
+    if(!list.length){
+     
+    }else{
+      listData = list.join('')
+      suggestions.innerHTML = listData
+    }
+  
+  }
+
+  const changeInputValue = (e) => {
+    
+  }
 
   return (
     <>
       <div className="section">
         <h1 className="title">Sponsor a Tree 🍃</h1>
-        <form className="form">
+        <form class="form">
+
           <div className="question">
             <label htmlFor="trees">
               <b>Tree Type</b>
             </label>
             <div className="drop-down-search">
               <div className="search-container">
-                <input
-                  type="text"
-                  name="tree-search"
-                  id="tree-input"
-                  onKeyUp={(e) => {
-                    showSuggestions(e);
-                  }}
-                  onClick={() => {
-                    toggleSuggestions();
-                  }}
-                  placeholder="Search tree species"
-                />
-                <svg
-                  className="search-svg"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="38"
-                  width="40"
-                >
-                  <path d="m38.65 40.1-12-12q-1.55 1.35-3.625 2.025Q20.95 30.8 19 30.8q-5.1 0-8.575-3.45-3.475-3.45-3.475-8.5t3.45-8.5q3.45-3.45 8.5-3.45 5 0 8.5 3.45t3.5 8.5q0 2.1-.7 4.1-.7 2-2.05 3.55L40.3 38.55q.35.35.35.8 0 .45-.4.8-.35.3-.8.3-.45 0-.8-.35Zm-19.7-11.5q4.15 0 6.975-2.8 2.825-2.8 2.825-6.95 0-4.2-2.825-7t-6.975-2.8q-4.2 0-7 2.8t-2.8 7q0 4.15 2.8 6.95t7 2.8Z" />
-                </svg>
+                <input type="text" name="tree-search" id="tree-input" onChange={(e) => {showSuggestions(e)}} placeholder="Search tree species" />
+                <svg className="search-svg"  xmlns="http://www.w3.org/2000/svg" height="38" width="40"><path d="m38.65 40.1-12-12q-1.55 1.35-3.625 2.025Q20.95 30.8 19 30.8q-5.1 0-8.575-3.45-3.475-3.45-3.475-8.5t3.45-8.5q3.45-3.45 8.5-3.45 5 0 8.5 3.45t3.5 8.5q0 2.1-.7 4.1-.7 2-2.05 3.55L40.3 38.55q.35.35.35.8 0 .45-.4.8-.35.3-.8.3-.45 0-.8-.35Zm-19.7-11.5q4.15 0 6.975-2.8 2.825-2.8 2.825-6.95 0-4.2-2.825-7t-6.975-2.8q-4.2 0-7 2.8t-2.8 7q0 4.15 2.8 6.95t7 2.8Z"/></svg>
                 <div id="suggestions">
-                  <options className="suggestion-items">1</options>
-                  <options className="suggestion-items">2</options>
-                  <options className="suggestion-items">3</options>
-                  <options className="suggestion-items">4</options>
-                  <options className="suggestion-items">5</options>
+    
                 </div>
               </div>
             </div>
---
+++ Client/src/pages/StartPlanting/index.jsx
@@ -0,0 +1,7 @@
+const index = () => {
+  return (
+    <div>Start Planting Progress Here</div>
+  )
+}
+
+export default index
